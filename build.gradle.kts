20bbb445c1f24e63d3689325ab0aab2df5287a8b
ivanlytovka <ivanlytovka@gmail.com>
2023-08-20 10:58:21 -0400

    feat: add pre-commit hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,8 @@ tasks.bootBuildImage {
             "BP_JVM_VERSION" to "17",
             "BP_SPRING_CLOUD_BINDINGS_DISABLED" to "true",
             "BPL_SPRING_CLOUD_BINDINGS_DISABLED" to "true",
-            "BPE_JAVA_TOOL_OPTIONS" to "-XX:ActiveProcessorCount=0 -XX:+UseContainerSupport -XX:+PreferContainerQuotaForCPUCount"
-        )
+            "BPE_JAVA_TOOL_OPTIONS" to "-XX:ActiveProcessorCount=0 -XX:+UseContainerSupport -XX:+PreferContainerQuotaForCPUCount",
+        ),
     )
 }
 
@@ -64,13 +64,26 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+// ---- pre-commit hook ----
+tasks.register("installGitHook", Copy::class) {
+    from(File(rootProject.rootDir, "scripts/pre-commit.sh"))
+    into(File(rootProject.rootDir, ".git/hooks"))
+    fileMode = 509
+    rename("pre-commit.sh", "pre-commit")
+}
+
+tasks.compileKotlin {
+    dependsOn("installGitHook")
+}
+
 spotless {
     java {
         removeUnusedImports()
     }
     kotlin {
-        target("**/*.kt")
         ktlint()
+        target("**/*.kt")
+        targetExclude("**/build/**")
     }
     kotlinGradle {
         target("**/*.gradle.kts")
